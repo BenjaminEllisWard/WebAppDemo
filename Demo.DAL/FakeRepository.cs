1e2faec476842a5281975003107a96cc951011d2
Ben Ward <benjamin.e.ward@outlook.com>
2019-04-26 15:45:11 -0400

    Clean up repository code.

--- Demo.DAL/FakeRepository.cs
+++ Demo.DAL/FakeRepository.cs
@@ -16,35 +16,24 @@ namespace Demo.DAL
         private List<ItemModelDTO> FakeContext;
 
         /// <summary>
-        /// Returns list of ItemModelDTO's
+        /// Contructor automatically populates FakeContext.
         /// </summary>
-        public List<ItemModelDTO> GetItems()
+        /// <param name="recordCount">Optional parameter. Intended to allow the user
+        /// to test performance by varying page size.</param>
+        public FakeRepository(int recordCount = 20)
         {
-            //TODO this method doesn't do anything other than return FakeContext. A parameter for a filter model
-            //can be added to add some logic if needed in the future.
-
-            var fakeContext = new List<ItemModelDTO>();
-
-            fakeContext = FakeContext.OrderBy(x => x.ItemId).ToList();
-
-            return fakeContext;
+            FakeContext = CreateRecordsForContextList(recordCount);
         }
 
-        private string PickANameForFakeContext(Random rnd)
+        /// <summary>
+        /// Selects a name at random from a bank.
+        /// </summary>
+        /// <param name="rnd"></param>
+        /// <returns></returns>
+        private string PickANameForSingleRecord(Random rnd)
         {
             var ng = new NameGenerator();
             return ng.PickAName(rnd);
-        }
-
-        
-        /// <summary>
-        /// Hard codes a fake context for demonstration purposes
-        /// </summary>
-        /// <param name="recordCount">Determines the amount of records in the fake context</param>        
-        private void CreateFakeContext(int recordCount)
-        {
-            var fakeContext = CreateRecordsForContextList(recordCount);
-            FakeContext = fakeContext;
         }
 
         /// <summary>
@@ -55,26 +44,25 @@ namespace Demo.DAL
         private List<ItemModelDTO> CreateRecordsForContextList(int recordCount)
         {
             var recordList = new List<ItemModelDTO>();
-            var basePrice = 1000.00M;
             var rnd = new Random();
 
             //Populates record with ItemModelDTOs that vary on id, name, and organization.
             for (int i = 0; i < recordCount / 3; ++i)
             {
-                recordList.Add(CreateSingleRecordForContextList(3*i, "ThisDescription", "Sales", basePrice, rnd));
-                recordList.Add(CreateSingleRecordForContextList(3*i + 1, "ThatDescription", "Accounting", basePrice, rnd));
-                recordList.Add(CreateSingleRecordForContextList(3*i + 2, "OtherDescription", "Marketing", basePrice, rnd));
+                recordList.Add(CreateSingleRecord(3*i, "ThisDescription", "Sales", rnd));
+                recordList.Add(CreateSingleRecord(3*i + 1, "ThatDescription", "Accounting", rnd));
+                recordList.Add(CreateSingleRecord(3*i + 2, "OtherDescription", "Marketing", rnd));
             }
 
             //The following blocks resolve cases where recordCount is not divisible by three, meaning
             //the block above has not completely finished creating the last one or two records.
             if (recordCount % recordList.Count() != 0)
             {
-                recordList.Add(CreateSingleRecordForContextList(recordCount - (recordCount % recordList.Count()), "ThisDescription", "Sales", basePrice, rnd));
+                recordList.Add(CreateSingleRecord(recordCount - (recordCount % recordList.Count()),"ThisDescription", "Sales", rnd));
             }
             if (recordCount % recordList.Count() == 1)
             {
-                recordList.Add(CreateSingleRecordForContextList(recordCount - 1, "ThatDescription", "Sales", basePrice, rnd));
+                recordList.Add(CreateSingleRecord(recordCount - 1, "ThatDescription", "Sales", rnd));
             }
 
             return recordList;
@@ -89,15 +77,15 @@ namespace Demo.DAL
         /// <param name="basePrice">Used to avoid Price == 0</param>
         /// <param name="rnd">Allows random variation between records.</param>
         /// <returns></returns>
-        private ItemModelDTO CreateSingleRecordForContextList(int counter, string description, string org, decimal basePrice, Random rnd)
+        private ItemModelDTO CreateSingleRecord(int counter, string description, string org, Random rnd)
         {
             var record = new ItemModelDTO
             {
                 ItemId = counter + 1,
                 ItemName = description + counter.ToString(),
-                Price = basePrice * rnd.Next(5, 21),
+                Price = 1000.00M * rnd.Next(5, 21),
                 Organization = org,
-                POCName = PickANameForFakeContext(rnd),
+                POCName = PickANameForSingleRecord(rnd),
                 DateEstablished = DateTime.Now.AddMonths(-rnd.Next(6, 25)),
                 DateBegin = DateTime.Now.AddMonths(rnd.Next(-6, 13))
             };
@@ -120,13 +108,13 @@ namespace Demo.DAL
         }
 
         /// <summary>
-        /// Contructor automatically populates FakeContext.
+        /// Returns list of ItemModelDTO's
         /// </summary>
-        /// <param name="recordCount">Optional parameter. Intended to allow the user
-        /// to test performance by varying page size.</param>
-        public FakeRepository(int recordCount = 20)
+        public List<ItemModelDTO> GetItems()
         {
-            CreateFakeContext(recordCount);
+            //TODO this method doesn't do anything other than return FakeContext. A parameter for a filter model
+            //can be added if needed in the future.
+            return FakeContext.OrderBy(x => x.ItemId).ToList();
         }
 
         #region IDisposable Support
